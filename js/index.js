eb71ca54b3d91f5503082e07c1187c6871b22385
airtonbjunior <airtonbjunior@gmail.com>
2024-10-01 08:59:46 -0300

    alterações aula 01-10

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 // TO-DO:
 // Organizar código-fonte,
+// Tratar botão de fechar do alerta de registro
 
 const diaSemana = document.getElementById("dia-semana");
 const diaMesAno = document.getElementById("dia-mes-ano");
@@ -14,6 +15,14 @@ const btnDialogFechar = document.getElementById("btn-dialog-fechar");
 btnDialogFechar.addEventListener("click", () => {
     dialogPonto.close();
 });
+
+
+const nextRegister = {
+    "entrada": "intervalo",
+    "intervalo": "volta-intervalo", 
+    "volta-intervalo": "saida", 
+    "saida": "entrada"
+}
 
 
 let registerLocalStorage = getRegisterLocalStorage();
@@ -38,28 +47,14 @@ function getCurrentPosition() {
 }
 
 
-const typeRegister = document.getElementById("tipos-ponto");
+
 
 const btnDialogBaterPonto = document.getElementById("btn-dialog-bater-ponto");
 btnDialogBaterPonto.addEventListener("click", () => {
-
+    const typeRegister = document.getElementById("tipos-ponto");
     let lastTypeRegister = localStorage.getItem("lastTypeRegister");
 
-    // TO-DO:
-    // Pq o select não está com a option correspondente?
-    if(lastTypeRegister == "entrada") {
-        console.log("lastTypeRegister é entrada");
-        typeRegister.value = "intervalo";
-    }
-    if(lastTypeRegister == "intervalo") {
-        typeRegister.value = "volta-intervalo";
-    }
-    if(lastTypeRegister == "volta-intervalo") {
-        typeRegister.value = "saida";
-    }
-    if(lastTypeRegister == "saida") {
-        typeRegister.value = "entrada"
-    }
+    console.log(lastTypeRegister);
 
     let ponto = {
         "data": getCurrentDate(),
@@ -73,25 +68,14 @@ btnDialogBaterPonto.addEventListener("click", () => {
 
     saveRegisterLocalStorage(ponto);
 
-    localStorage.setItem("lastTypeRegister", typeRegister.value);
+    
     localStorage.setItem("lastDateRegister", ponto.data);
     localStorage.setItem("lastTimeRegister", ponto.hora);
 
     dialogPonto.close();
 
-    // TO-DO:
-    // CRIAR UM ALERTA NO TOPO DA PÁGINA PRINCIPAL PARA CONFIRMAR O REGISTRO DE PONTO
-    // DEVE FICAR ABERTO POR 3 SEGUNDOS E DEVE TER UM EFEITO DE TRANSIÇÃO
-    // DEVE PODER SER FECHADO PELO USUÁRIO QUE NÃO QUISER AGUARDAR 3s
-    // DEVE MOSTRAR UMA MENSAGEM DE SUCESSO AO REGISTRAR O PONTO
-    // CASO OCORRA ALGUM ERRO, MOSTRAR NO ALERTA 
-    // AS CORES DEVEM SER DIFERENTES EM CASO DE SUCESSO/ERRO/ALERTA
-
     divAlertaRegistroPonto.classList.remove("hidden");
     divAlertaRegistroPonto.classList.add("show");
-    
-    // TO-DO:
-    // fazer um efeito de transição para o alerta
 
     setTimeout(() => {
         divAlertaRegistroPonto.classList.remove("show");
@@ -102,8 +86,10 @@ btnDialogBaterPonto.addEventListener("click", () => {
 
 
 function saveRegisterLocalStorage(register) {
+    const typeRegister = document.getElementById("tipos-ponto");
     registerLocalStorage.push(register); // Array
     localStorage.setItem("register", JSON.stringify(registerLocalStorage));
+    localStorage.setItem("lastTypeRegister", typeRegister.value);
 } 
 
 
@@ -122,23 +108,18 @@ function getRegisterLocalStorage() {
 // TO-DO:
 // alterar o nome da função
 function register() {
-    // TO-DO:
-    // Atualizar hora a cada segundo e data 00:00:00
     dialogData.textContent = "Data: " + getCurrentDate();
     dialogHora.textContent = "Hora: " + getCurrentHour();
-
-
-    // TO-DO:
-    // Verificar se há último registro. Se não houver, tratar o que será escrito na base do dialog
-    // Opções: escrever "Sem registros anteriores" ou não escrever nada
-
-
-    let lastRegisterText = "Último registro: " + localStorage.getItem("lastDateRegister") + " - " + localStorage.getItem("lastTimeRegister") + " | " + localStorage.getItem("lastTypeRegister")
-    document.getElementById("dialog-last-register").textContent = lastRegisterText;
+    
+    let lastTypeRegister = localStorage.getItem("lastTypeRegister");
+    if(lastTypeRegister) {
+        const typeRegister   = document.getElementById("tipos-ponto");
+        typeRegister.value   = nextRegister[lastTypeRegister];
+        let lastRegisterText = "Último registro: " + localStorage.getItem("lastDateRegister") + " - " + localStorage.getItem("lastTimeRegister") + " | " + localStorage.getItem("lastTypeRegister")
+        document.getElementById("dialog-last-register").textContent = lastRegisterText;
+    }
 
     dialogPonto.showModal();
-
-    console.log(localStorage.getItem("lastTypeRegister"));
 }
 
 function getWeekDay() {
@@ -154,22 +135,8 @@ function getCurrentHour() {
 
 
 function getCurrentDate() {
-    // TO-DO:
-    // Alterar a solução para considerar padStart ou slice
-    // Considerar formatos diferentes da data, conforme localização
-    // do usuário dd/mm/aaaa, mm/dd/aaaa, aaaa/mm/dd, aaaa.mm.dd
-    // Verificar se no Date() há algum método que possa auxiliar
-    // locale
     const date = new Date();
-    let month = date.getMonth();
-    let day = date.getDate();
-    if (day < 10) {
-        day = "0" + day
-    }
-    if (month < 10) {
-        month = "0" + (month + 1)
-    }
-    return day + "/" + month + "/" + date.getFullYear();
+    return String(date.getDate()).padStart(2, '0') + "/" + String((date.getMonth() + 1)).padStart(2, '0') + "/" + String(date.getFullYear()).padStart(2, '0');
 }
 
 function printCurrentHour() {
